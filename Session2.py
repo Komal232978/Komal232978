8f76e8151e717bef206b10f2b5236ae6b8efa7f5
Komal232978 <232978@students.aih.nsw.edu.au>
2024-05-29 19:05:42 +1000

    Create Session2.py
    practice commit first program

---
+++ Session2.py
@@ -0,0 +1,10 @@
+# Simple Calc
+First_Num = 6 # gave the first number value
+Sec_Num = 2 # assigned the second number
+divisions = First_Num / Sec_Num
+print(divisions)
+3.0
+mul = First_Num * Sec_Num
+Sub = First_Num - Sec_Num
+add = First_Num + Sec_Num
+print(divisions, mul, Sub, add)
